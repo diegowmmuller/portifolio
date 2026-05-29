93619f051349fd5694f21fb059e2d041aa6e2f50
diegowmmuller <diegomaglia96@gmail.com>
2026-05-29 16:41:58 -0300

    feat/analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { AboutSection } from './components/AboutSection';
 import { ProjectSection } from './components/ProjectSection';
 import { SkillSection } from './components/SkillSection';
 import { Footer } from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 				<SkillSection />
 			</MainContainer>
 			<Footer />
+			<Analytics />
 		</AppLayout>
 	);
 }
